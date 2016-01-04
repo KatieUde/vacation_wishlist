0c9d1e0641b94a98eaf5d6654acd28e3085a32ff
Katherine Ude <Katie.Ude@gmail.com>
2016-01-03 20:28:31 -0600

    saving updated work

--- public/javascripts/app.views.js
+++ public/javascripts/app.views.js
@@ -4,7 +4,7 @@ var active = active || {};
 app.addVacationView = Backbone.View.extend({
   el: $("#vacation_tag"),
   events: {
-    'click button': 'addVacation'
+    "click button": "addVacation"
   },
   intialize: function() {
     console.log("add VacationView initialized");
@@ -22,3 +22,34 @@ app.addVacationView = Backbone.View.extend({
     this.collection.create(vacation);
   }
 });
+
+app.ModelView = Backbone.View.extend({
+  initalize: function() {
+    this.render();
+  },
+  render: function() {
+    var data = this.model.attributes;
+    var template = $("#vacation_template").html();
+    var compileTemplate = _.template(template);
+    var html = compileTemplate(data);
+    this.$el.append(html);
+  }
+}); // end of ModelView Function
+
+app.CollectionView = Backbone.View.extend({
+  el: $("#vacation_list_tag"),
+  initialize: function() {
+    this.$el.empty();
+    this.render();
+  },
+  render: function() {
+    this.$el.empty();
+    var models = this.collection.models;
+    for (var model in models) {
+      new app.ModelView({
+        model: models[model],
+        el: this.el
+      })
+    }
+  }
+})
